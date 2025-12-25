6fa79a09b9a2b3664a660055f4bc6e9cb01d6ff8
Josh Waterhouse <deepfriedfroglegs@gmail.com>
2025-12-25 02:26:01 +0000

    enable docker and add user to docker group

--- configuration.nix
+++ configuration.nix
@@ -1,40 +1,44 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  imports = [  ];
+    imports = [  ];
   
-  nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nixpkgs.config.allowUnfree = true;
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
   
-  wsl.defaultUser = "me";
+    wsl.defaultUser = "me";
 
-  system.stateVersion = "25.05"; 
+    system.stateVersion = "25.05"; 
 
-  environment.systemPackages = with pkgs; [
-    beam28Packages.erlang
-    beam28Packages.elixir_1_19
-    docker
-    eza
-    lazygit
-    oh-my-zsh
-    ripgrep
-    starship
-    (inputs.yazi.packages.${stdenv.hostPlatform.system}.default.override {
-      _7zz = pkgs._7zz-rar;
-    })
-  ];
 
-  programs.zsh = {
-    enable = true;
-    ohMyZsh = {
-      enable = true;
+    environment.systemPackages = with pkgs; [
+        beam28Packages.erlang
+        beam28Packages.elixir_1_19
+        eza
+        lazygit
+        oh-my-zsh
+        ripgrep
+        starship
+        (inputs.yazi.packages.${stdenv.hostPlatform.system}.default.override {
+        _7zz = pkgs._7zz-rar;
+        })
+    ];
+
+    programs.zsh = {
+        enable = true;
+        ohMyZsh = {
+        enable = true;
+        };
     };
-  };
-  users.defaultUserShell = pkgs.zsh;
+    users.defaultUserShell = pkgs.zsh;
 
-  programs.neovim.enable = true;
+    users.users.me.extraGroups = [ "docker" ];
 
-  programs.git.enable = true;
+    virtualisation.docker.enable = true;
 
-  programs.direnv.enable = true;
+    programs.neovim.enable = true;
+
+    programs.git.enable = true;
+
+    programs.direnv.enable = true;
 }
